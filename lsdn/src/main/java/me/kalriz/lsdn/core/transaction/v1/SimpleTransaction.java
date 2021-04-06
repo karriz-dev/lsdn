806c217c388887b5facf5a5c6316e7d1fd6dc624
ldh1428a <ldh1428a@sogang.ac.kr>
2021-04-06 17:19:49 +0900

    transaction structure impl

---
+++ lsdn/src/main/java/me/kalriz/lsdn/core/transaction/v1/SimpleTransaction.java
@@ -0,0 +1,18 @@
+package me.kalriz.lsdn.core.transaction.v1;
+
+import java.util.Map;
+
+import lombok.Builder;
+import lombok.Getter;
+import me.kalriz.lsdn.core.transaction.Transaction;
+
+@Getter
+public class SimpleTransaction extends Transaction {
+
+	@Builder
+	public SimpleTransaction(String txHash, String merkleHash, long blockHeight, String from,
+			String to, String speedy, String maxSpeedy, String value, Map<String, String> data) {
+		super(txHash, System.currentTimeMillis(), 0x00000000, merkleHash, blockHeight, from, to, speedy, maxSpeedy, value, data);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ lsdn/src/test/java/me/kalriz/lsdn/core/transaction/v1/SimpleTransactionTests.java
@@ -0,0 +1,44 @@
+package me.kalriz.lsdn.core.transaction.v1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.gson.Gson;
+
+import me.kalriz.lsdn.core.transaction.Transaction;
+
+public class SimpleTransactionTests {
+	@Test
+	public void simpleTransactionTest() {
+		Transaction tx = SimpleTransaction.builder().merkleHash("0x00").blockHeight(0L).from("0x00").to("0x00")
+				.speedy("000000000000000000000").maxSpeedy("000000000000000000000").value("000000000000001000000")
+				.data(null).build();
+
+		// data check
+		assertEquals(tx.getBlockHeight(), 0L);
+		assertEquals(tx.getMerkleHash(), "0x00");
+		assertEquals(tx.getFrom(), "0x00");
+		assertEquals(tx.getTo(), "0x00");
+		assertEquals(tx.getValue(), "000000000000001000000");
+		assertEquals(tx.getData(), null);
+	}
+
+	@Test
+	public void simpleTransactionJsonTest() {
+		Transaction tx = SimpleTransaction.builder().merkleHash("0x00").blockHeight(0L).from("0x00").to("0x00")
+				.speedy("000000000000000000000").maxSpeedy("000000000000000000000").value("000000000000001000000")
+				.data(null).build();
+
+		// json check
+		Gson gson = new Gson();
+
+		String jsonObj = gson.toJson(tx);
+
+		Transaction txObj = gson.fromJson(jsonObj, SimpleTransaction.class);
+
+		// data check
+		assertEquals(txObj.getMerkleHash(), "0x00");
+		assertEquals(txObj.getValue(), "000000000000001000000");
+	}
+}
